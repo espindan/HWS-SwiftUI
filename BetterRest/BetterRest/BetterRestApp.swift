9670b7188c46a449129e33cb3f437af855d1fdfa
Danny Espinoza <danny.espinozasanchez@fiserv.com>
2023-03-24 09:26:20 -0600

    Add project Better Rest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Danny Espinoza Sanchez on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
