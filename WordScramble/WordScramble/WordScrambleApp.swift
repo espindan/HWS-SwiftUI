e323f7930a85a63237d6e5e881fcd2231249512a
Danny Espinoza <danny.espinozasanchez@fiserv.com>
2023-04-21 12:01:27 -0600

    Add WordScramble project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Danny Espinoza Sanchez on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
